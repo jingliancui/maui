ca5126a133669da2972efcd75cf45a147ed37c3a
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-21 13:44:57 +0200

    Add more tests

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11869.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue11869 : _IssuesUITest
+{
+	public Issue11869(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "[Bug] ShellContent.IsVisible issue on Android";
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void IsVisibleWorksForShowingHidingTabs()
+	{
+		RunningApp.WaitForElement("TopTab2");
+		RunningApp.Tap("HideTop2");
+		RunningApp.WaitForNoElement("TopTab2");
+
+		RunningApp.WaitForElement("TopTab3");
+		RunningApp.Tap("HideTop3");
+		RunningApp.WaitForNoElement("TopTab3");
+
+		RunningApp.WaitForElement("Tab 2");
+		RunningApp.Tap("HideBottom2");
+		RunningApp.WaitForNoElement("Tab 2");
+
+		RunningApp.WaitForElement("Tab 3");
+		RunningApp.Tap("HideBottom3");
+		RunningApp.WaitForNoElement("Tab 3");
+
+		RunningApp.Tap("ShowAllTabs");
+		RunningApp.WaitForElement("TopTab2");
+		RunningApp.WaitForElement("TopTab3");
+		RunningApp.WaitForElement("Tab 2");
+		RunningApp.WaitForElement("Tab 3");
+	}
+}
