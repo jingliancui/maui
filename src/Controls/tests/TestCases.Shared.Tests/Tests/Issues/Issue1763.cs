a1137588db48dad1af6f6cc4b5f3bb1689b40c7f
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-21 14:38:26 +0200

    Add more tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue1763.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue1763.cs
@@ -12,16 +12,16 @@ public class Issue1763 : _IssuesUITest
 
 	public override string Issue => "First item of grouped ListView not firing .ItemTapped";
 
-	[Test]
-	[Category(UITestCategories.ListView)]
-	[FailsOnIOS]
-	[FailsOnAndroid]
-	public void TestIssue1763ItemTappedFiring()
-	{
-		RunningApp.WaitForElement(q => q.Marked("Contacts"));
-		RunningApp.Tap(q => q.Marked("Egor1"));
-		RunningApp.WaitForElement(q => q.Marked("Tapped a List item"));
-		RunningApp.Tap(q => q.Marked("Destruction"));
-		RunningApp.WaitForElement(q => q.Marked("Contacts"));
-	}
+	// [Test]
+	// [Category(UITestCategories.ListView)]
+	// [FailsOnIOS]
+	// [FailsOnAndroid]
+	// public void TestIssue1763ItemTappedFiring()
+	// {
+	// 	RunningApp.WaitForElement(q => q.Marked("Contacts"));
+	// 	RunningApp.Tap(q => q.Marked("Egor1"));
+	// 	RunningApp.WaitForElement(q => q.Marked("Tapped a List item"));
+	// 	RunningApp.Tap(q => q.Marked("Destruction"));
+	// 	RunningApp.WaitForElement(q => q.Marked("Contacts"));
+	// }
 }
